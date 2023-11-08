d885dc7c4b673566d6f61a67fe67f4fc774f6c5e
Jerico <jericodcosta95375788@gmail.com>
2023-11-08 12:50:49 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const arr = ["A", "B", "C", "D", "E"];
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        {arr.map((val, index) => (
+          <li key={`relativeListItem${index + 1}`}>{val}</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
